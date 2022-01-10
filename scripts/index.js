9ef972217a49fc7a56a0eefdaf25affabb3296d1
Olesea Cotorobai <olesea.cotorobai@mail.ru>
2022-01-10 18:51:07 +0100

    changed js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,41 @@
 let buttonProfileEdit = document.querySelector(".profile__edit-button");
 let buttonClose = document.querySelector(".popup__close-button");
 let overlay = document.querySelector(".overlay");
+let buttonSubmit = document.querySelector(".popup__submit-button");
+let overlayActiveClass = "overlay_active";
 const formElement = document.querySelector(".popup__form");
 const popupProfile = document.querySelector(".popup");
 const profileSubmitButton = document.querySelector(".popup__submit-button");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
+let formNameInput = formElement.querySelector(".popup__name");
+let formDescriptionInput = formElement.querySelector(".popup__description");
 
-function openPopup(popup) {
-  popup.classList.add("overlay_active");
-}
-function closePopup(popup) {
-  popup.classList.remove("overlay_active");
-}
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  let formNameInput = formElement.querySelector(".popup__name");
-  let formDescriptionInput = formElement.querySelector(".popup__description");
-  profileName.textContent =  formNameInput.value;
-  profileDescription.textContent = formDescriptionInput.value;
-  closePopup(popupProfile);
-}
-
-buttonClose.addEventListener("click", () => {
-  closePopup(popupProfile);
-});
+buttonProfileEdit.addEventListener("click", function (event) {
+    
+    overlay.classList.add(overlayActiveClass);
+  });
+  buttonClose.addEventListener("click", function () {
+    overlay.classList.remove(overlayActiveClass);
+  });
 buttonProfileEdit.addEventListener("click", () => {
   openPopup(popupProfile);
 });
-profileSubmitButton.addEventListener("submit", formSubmitHandler);
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  
+  profileName.textContent =  formNameInput.value;
+ profileDescription.textContent = formDescriptionInput.value;
+  closePopup(popupProfile);
+}
+buttonClose.addEventListener("click", () => {
+  closePopup(popupProfile);
+});
+buttonSubmit.addEventListener("submit", formSubmitHandler);
+
